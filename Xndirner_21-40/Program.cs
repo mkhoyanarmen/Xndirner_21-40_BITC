6df48862a9e0002dca41903b586be29418f235a3
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-06-07 12:23:30 +0400

    Add project files.

---
+++ Xndirner_21-40/Program.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xndirner_21_40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //21
+            //int a = 45, b = 1, c = -50, max = 0;
+
+            //if (a > b && a > c)
+            //{
+            //    max = a;
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    max = b;
+            //}
+            //else
+            //{
+            //    max = c;
+            //}
+            //Console.WriteLine($"Max: {max}");
+
+            //22
+            //int a = 45, b = 1, c = -50, min = 0;
+
+            //if (a < b && a < c)
+            //{
+            //    min = a;
+            //}
+            //else if (b < a && b < c)
+            //{
+            //    min = b;
+            //}
+            //else
+            //{
+            //    min = c;
+            //}
+            //Console.WriteLine($"Min: {min}");
+
+            //23
+            //int a = 45, b = 1, c = -50;
+            //Console.WriteLine(a == 1 || b == 1 || c == 1 ? true : false);
+
+            //24
+            //int a = 45, b = 1, c = -50;
+            //if ((a == 2 && b == 2) || (a == 2 && c == 2) || (b == 2 && c == 2))
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+            //25
+            //int a = 45, b = 15, c = 50, max = 0;
+
+            //if (a > b && a > c)
+            //{
+            //    max = a;
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    max = b;
+            //}
+            //else
+            //{
+            //    max = c;
+            //}
+
+            //if (2 * max < a + b + c)
+            //{
+            //    Console.WriteLine("y = 1");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("y = 2");
+            //}
+
+            //26
+            //int a = 45, b = 15, c = 50;
+            //if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0)
+            //{
+            //    Console.WriteLine(1);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(2);
+            //}
+
+            //27
+            //int a = 45, b = 60, c = 70;
+            //if (b - a == c - b)
+            //{
+            //    Console.WriteLine(1);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(2);
+            //}
+
+            //28
+            //double a = 5, b = 15, c = 45;
+            //if (b / a == c / b)
+            //{
+            //    Console.WriteLine(1);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(2);
+            //}
+
+            //29
+            //int a = -85, b = -60, c = 23;
+            //if (a > b && a > c)
+            //{
+            //    if (b > c)
+            //        Console.WriteLine($"{c} {b} {a}");
+            //    else
+            //        Console.WriteLine($"{b} {c} {a}");
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    if (a > c)
+            //        Console.WriteLine($"{c} {a} {b}");
+            //    else
+            //        Console.WriteLine($"{a} {c} {b}");
+            //}
+            //else
+            //{
+            //    if (a > b)
+            //        Console.WriteLine($"{b} {a} {c}");
+            //    else
+            //        Console.WriteLine($"{a} {b} {c}");
+            //}
+
+            //30
+            //int a = 45, b = -60, c = 23;
+            //if (a > b && a > c)
+            //{
+            //    if (b > c)
+            //        Console.WriteLine($"{a} {b} {c}");
+            //    else
+            //        Console.WriteLine($"{a} {c} {b}");
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    if (a > c)
+            //        Console.WriteLine($"{b} {a} {c}");
+            //    else
+            //        Console.WriteLine($"{b} {c} {a}");
+            //}
+            //else
+            //{
+            //    if (a > b)
+            //        Console.WriteLine($"{c} {a} {b}");
+            //    else
+            //        Console.WriteLine($"{c} {b} {a}");
+            //}
+
+            //31
+            //int a = 45, b = 789, c = -852, d = 0, max = 0;
+            //if (a > b && a > c && a > d)
+            //{
+            //    max = a;
+            //}
+            //else if (b > a && b > c && b > d)
+            //{
+            //    max = b;
+            //}
+            //else if (c > a && c > b && c > d)
+            //{
+            //    max = c;
+            //}
+            //else
+            //{
+            //    max = d;
+            //}
+            //Console.WriteLine($"Max: {max}");
+
+            //32
+            //int a = 45, b = 789, c = -852, d = 0, min = 0;
+            //if (a < b && a < c && a < d)
+            //{
+            //    min = a;
+            //}
+            //else if (b < a && b < c && b < d)
+            //{
+            //    min = b;
+            //}
+            //else if (c < a && c < b && c < d)
+            //{
+            //    min = c;
+            //}
+            //else
+            //{
+            //    min = d;
+            //}
+            //Console.WriteLine($"Min: {min}");
+
+            //33
+            //int a = 45, b = 1, c = -852, d = 0;
+            //if (a == 1 || b == 1 || c == 1 || d == 1)
+            //    Console.WriteLine(true);
+            //else
+            //    Console.WriteLine(false);
+
+            //34
+            //int a = 45, b = 1, c = -852, d = 0;
+            //if (a + b == c + d || a + c == b + d || a + d == b + c)
+            //    Console.WriteLine(true);
+            //else
+            //    Console.WriteLine(false);
+
+            //35
+            //int a = 45, b = 1, c = -852, d = 0;
+            //if (a == b + c + d || b == a + c + d || c == a + b + d || d == a + b + c)
+            //    Console.WriteLine(true);
+            //else
+            //    Console.WriteLine(false);
+
+            //36
+            //int a = 45, b = 1, c = -852, d = 0;
+            //if ((a % 2 != 0 && b % 2 != 0) 
+            //    || (a % 2 !=0 && c % 2!=0) 
+            //    || (a % 2 != 0 && d % 2 != 0)
+            //    || (b % 2 != 0 && c % 2 != 0) 
+            //    || (b % 2 != 0 && d % 2 != 0) 
+            //    || (c % 2 != 0 && d % 2 != 0))
+            //    Console.WriteLine(1);
+            //else
+            //    Console.WriteLine(2);
+
+            //37
+            //int a = 45, b = 1, c = -852, d = 0;
+            //if (d - c == c - b && c - b == b - a)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //    Console.WriteLine(false);
+
+            //38
+            //double a = 45, b = 1, c = -852, d = 0;
+            //if (d / c == c / b && c / b == b / a)
+            //{
+            //    Console.WriteLine(true);
+            //}
+            //else
+            //    Console.WriteLine(false);
+
+            //39
+            //int a = 45, b = 1, c = -852, d = 0;
+            //int[] arr = { a, b, c, d };
+            //Array.Sort(arr);
+            //foreach (var item in arr)
+            //{
+            //    Console.Write(item + " ");
+            //}
+            //Console.WriteLine();
+
+            //40
+            int a = 45, b = 1, c = -852, d = 0;
+            int[] arr = { a, b, c, d };
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
